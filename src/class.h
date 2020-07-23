654831080bf2ee01d1d8e15f3b99f6b4e89313fa
Rieon Ke <rieon@rieon.cn>
2020-07-23 15:14:05 +0800

    类注解头定义

---
+++ src/class.h
@@ -0,0 +1,14 @@
+//
+// Created by Rieon Ke on 2020/7/23.
+//
+
+#ifndef CJASM_CLASS_H
+#define CJASM_CLASS_H
+
+#include <cjasm.h>
+#include "annotation.h"
+
+bool cj_class_add_annotation(cj_class_t *ctx, cj_annotation_t *ann, bool visible);
+
+
+#endif //CJASM_CLASS_H

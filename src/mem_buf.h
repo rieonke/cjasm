1b7da525605b0faf267dcc570616b3e54e29a13a
Rieon Ke <rieon@rieon.cn>
2020-07-30 17:19:36 +0800

    缓冲区:添加buf_ps定义

--- src/mem_buf.h
+++ src/mem_buf.h
@@ -11,6 +11,7 @@
 #define CJ_BUFFER_SIZE 1024
 #endif
 
+typedef void *cj_mem_buf_pos_t;
 typedef struct cj_mem_buf_s cj_mem_buf_t;
 
 struct cj_mem_buf_s {
@@ -48,6 +49,8 @@ void cj_mem_buf_write_str(cj_mem_buf_t *buf, char *str, int len);
 void cj_mem_buf_write_buf(cj_mem_buf_t *buf, cj_mem_buf_t *buf1);
 
 void cj_mem_buf_free(cj_mem_buf_t *buf);
+
+cj_mem_buf_pos_t *cj_mem_buf_pos(cj_mem_buf_t *buf);
 
 
 #endif //CJASM_MEM_BUF_H

cd39bac6ca3b619b84827b25abf718c1190355dd
Rieon Ke <rieon@rieon.cn>
2020-08-01 10:53:38 +0800

    buf:添加测试

---
+++ test/mem_buf_rw.c
@@ -0,0 +1,30 @@
+//
+// Created by Rieon Ke on 2020/7/31.
+//
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include "../src/mem_buf.h"
+
+
+void test_mem_buf(void **state) {
+    cj_mem_buf_t *buf = cj_mem_buf_new();
+    cj_mem_buf_printf(buf, "%s\n", "hello world");
+    cj_mem_buf_flush(buf);
+    assert_true(strncmp("hello world\n", (char*)buf->data, 12));
+//    assert_string_equal("hello world\n", buf->data);
+    cj_mem_buf_free(buf);
+}
+
+
+int main(void) {
+
+    const struct CMUnitTest tests[] = {
+            cmocka_unit_test(test_mem_buf),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}

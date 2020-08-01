cc505e1c659f87044c2e28045c865697a3c2f340
Rieon Ke <rieon@rieon.cn>
2020-08-01 11:02:22 +0800

    buf:添加测试

--- test/mem_buf_rw.c
+++ test/mem_buf_rw.c
@@ -13,9 +13,9 @@
 void test_mem_buf(void **state) {
     cj_mem_buf_t *buf = cj_mem_buf_new();
     cj_mem_buf_printf(buf, "%s\n", "hello world");
+    cj_mem_buf_write_u1(buf,0);
     cj_mem_buf_flush(buf);
-    assert_true(strncmp("hello world\n", (char*)buf->data, 12));
-//    assert_string_equal("hello world\n", buf->data);
+    assert_string_equal("hello world\n", buf->data);
     cj_mem_buf_free(buf);
 }
 
